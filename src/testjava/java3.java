9cc7715a999ce337b2edffa93fe10d6bc28e3d87
Administrator <Administrator@192.168.66.203>
2019-06-12 23:03:34 +0800

    1

--- src/testjava/java3.java
+++ src/testjava/java3.java
@@ -5,6 +5,7 @@ public class java3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
 
 	}
 
