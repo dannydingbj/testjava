669351ae5173feb4b453d7794c46dfed64da3d0c
Administrator <Administrator@192.168.66.203>
2019-06-12 22:58:40 +0800

    1

--- src/testjava/java3.java
+++ src/testjava/java3.java
@@ -1,12 +1,10 @@
 package testjava;
 
-public class Test {
+public class java3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("hello word");
-		System.out.println("hello word2");
 	}
 
 }
---
+++ src/testjava2/testa.java
@@ -0,0 +1,11 @@
+package testjava2;
+
+public class testa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("next hello word");
+	}
+
+}
